7c4fc86145e628b8a5ef08b343e779a319a1b9e4
Chris Marshall <cmarshall10450@gmail.com>
2017-09-26 16:44:20 +0100

    test_number_to_full_month_name() passing

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -31,5 +31,30 @@ def add_string_as_number(string_1, string_2)
 end
 
 def number_to_full_month_name(num)
-
+	case num
+		when 1
+			"January"
+		when 2
+			"February"
+		when 3
+			"March"
+		when 4
+			"April"
+		when 5
+			"May"
+		when 6
+			"June"
+		when 7
+			"July"
+		when 8
+			"August"
+		when 9
+			"September"
+		when 10
+			"October"
+		when 11
+			"November"
+		when 12
+			"December"
+	end
 end
