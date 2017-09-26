8b7d801c19718a30efa2d0141eda5fc81b0a32a2
Chris Marshall <cmarshall10450@gmail.com>
2017-09-26 17:08:21 +0100

    Written test_fahrenheit_to_celsius()

--- ruby_functions_spec.rb
+++ ruby_functions_spec.rb
@@ -82,7 +82,8 @@ class Functions_Practice < MiniTest::Test
 
   #Given a value in farenheit, convert this into celsius.
   def test_fahrenheit_to_celsius()
-    #add test code here
+    result = fahrenheit_to_celsius(100)
+    assert_equal(37.7778, result)
   end
 
 
